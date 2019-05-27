fe6d07487801e7ba8c33932a81b873ff21248e95
Otavio Santana <otaviopolianasantana@gmail.com>
2019-05-27 08:39:29 -0300

    creates MySQL format

--- src/main/java/sh/platform/config/MySQL.java
+++ src/main/java/sh/platform/config/MySQL.java
@@ -1,4 +1,28 @@
 package sh.platform.config;
 
-public class MySQL {
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * A credential specialization that provides a {@link DataSource} using MySQL database
+ */
+public class MySQL extends SQLDatabase implements Supplier<DataSource> {
+
+    public MySQL(Map<String, Object> config) {
+        super(config);
+    }
+
+    @Override
+    public DataSource get() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setDatabaseName(getPath());
+        dataSource.setUser(getUserName());
+        dataSource.setPassword(getPassword());
+        dataSource.setPort(getPort());
+        dataSource.setServerName(getHost());
+        return dataSource;
+    }
 }
