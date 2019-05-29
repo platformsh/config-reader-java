30ac3d843f4e0d4d38ee8aea1cd7985543b3b7fa
Otavio Santana <otaviopolianasantana@gmail.com>
2019-05-29 10:38:30 -0300

    improves CredentialFormater documentation

--- src/main/java/sh/platform/config/CredentialFormatter.java
+++ src/main/java/sh/platform/config/CredentialFormatter.java
@@ -4,12 +4,12 @@ import java.util.Map;
 import java.util.function.Function;
 
 /**
- * In some cases the library being used to connect to a service wants its credentials formatted in a specific way;
- * it could be a DSN string of some sort or it needs certain values concatenated to the database name, etc.
- * For those cases you can use "Credential Formatters".
+ * <p> In some cases the library being used to connect to a service wants its credentials formatted in a specific way;</p>
+ * <p>it could be a DSN string of some sort or it needs certain values concatenated to the database name, etc.</p>
+ * <p>For those cases you can use "Credential Formatters".</p>
  *
- * A Credential Formatter is a functional interface that takes a credentials array and returns any type, since the
- * library may want different types.
+ * <p>A Credential Formatter is a functional interface that takes a credentials array and returns any type, since the
+ * library may want different types.</p>
  *
  * @param <T> the produced type
  */
